a00a1358bd99de938f26b337b86a0e65b5c55f57
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-01-10 07:06:51 +0900

    feat: design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
+    <div className='flex flex-col items-center font-mono'>
+      <div className='text-center pt-12'>
+        <p className='text-3xl'>Sharein</p>
+        <p>Easily share your images.</p> <br />
+        <div className='w-96 h-48 bg-slate-200 border-4 border-dashed border-slate-500'>
+          <p className='mt-20 text-slate-400'>Drag & drop here</p>
+        </div>
+      </div>
+
+      <div className='fixed bottom-0'>
+        <p>
+          Made with <span className='text-red-500'>♥</span> by <a href='https://github.com/kellsthepenguin'>kellsthepenguin</a>
+        </p>
+      </div>
+    </div>
   )
 }
